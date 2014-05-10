b562d628cef86634ecd2cb76f46c9fe073087f86
Harmon Brown <harmon@auduboncreek.com>
2014-05-10 14:52:51 -0500

    plot3.r revision
    I just added more text explaining why I made the variables the way I
    did.

--- plot3.R
+++ plot3.R
@@ -9,13 +9,19 @@ data<-subset(temp, temp$Date=="1/2/2007" | temp$Date=="2/2/2007")
 datetime<-paste(data$Date, data$Time)
 datetime<-as.POSIXct(datetime, format="%d/%m/%Y %H:%M:%S")
 
-## Make the line plot of Energy sub metering as a png
+## Make the variables and data frames to make the plot.  I tried to make this
+## plot just using the datetime variable and different versions on nrg, but
+## R would not do it because the x and y were different lengths.  The only way I
+## could get it work was to make data frames of both variables.  There is probably
+## a much more elegant and/or efficient way to do this, though.
 
 nrg1<-as.numeric(data$Sub_metering_1)
 nrg2<-as.numeric(data$Sub_metering_2)
 nrg3<-as.numeric(data$Sub_metering_3)
 nrg<-data.frame(nrg1,nrg2,nrg3)
 datetime.df<-data.frame(datetime,datetime,datetime)
+
+## Make the line plot of Energy sub metering as a png
 png(file="pngplot3.png")
 plot(datetime.df[,1],nrg[,1],type="n",xlab="", ylab="Energy sub metering")
 points(datetime.df[,1],nrg[,1],type='l')
