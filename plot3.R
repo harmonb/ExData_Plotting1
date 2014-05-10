908a3f1d5a32b83e7ab552a8fc9664ee9c2a3caa
Harmon Brown <harmon@auduboncreek.com>
2014-05-10 14:44:49 -0500

    plot 3
    Figure and code to make plot 3

---
+++ plot3.R
@@ -0,0 +1,26 @@
+##Read in file
+temp<-read.table("household_power_consumption.txt", sep=";", header=T, 
+                 stringsAsFactors=F)
+
+##Subset data
+data<-subset(temp, temp$Date=="1/2/2007" | temp$Date=="2/2/2007")
+
+## Create a variable of date and time and convert to POSIXct
+datetime<-paste(data$Date, data$Time)
+datetime<-as.POSIXct(datetime, format="%d/%m/%Y %H:%M:%S")
+
+## Make the line plot of Energy sub metering as a png
+
+nrg1<-as.numeric(data$Sub_metering_1)
+nrg2<-as.numeric(data$Sub_metering_2)
+nrg3<-as.numeric(data$Sub_metering_3)
+nrg<-data.frame(nrg1,nrg2,nrg3)
+datetime.df<-data.frame(datetime,datetime,datetime)
+png(file="pngplot3.png")
+plot(datetime.df[,1],nrg[,1],type="n",xlab="", ylab="Energy sub metering")
+points(datetime.df[,1],nrg[,1],type='l')
+points(datetime.df[,1],nrg[,2],type='l',col="red")
+points(datetime.df[,1],nrg[,3],type='l',col="blue")
+legend("topright", pch="-",col=c("black","blue","red"), 
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
